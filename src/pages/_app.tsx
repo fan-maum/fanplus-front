a6c8f86df24b465e877ea39b264e3f21e92c14d2
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-07-11 12:13:31 +0900

    수정: 제휴문의 페이지

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
-// export const SideBarContext = createContext<SideBarContextType | null>(null);
-
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
