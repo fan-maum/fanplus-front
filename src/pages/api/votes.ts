2671e52cbf8ed76b38896b97cbcf27a7a4b78b40
Kyeongeun Park <kellypark2032@gmail.com>
2023-07-13 11:22:08 +0900

    fix: vote.ts type

--- src/pages/api/votes.ts
+++ src/pages/api/votes.ts
@@ -2,18 +2,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getVotes } from '@/api/Vote';
 
-async function test(vote_type, page, per_page) {
-  const initialRowData = await getVotes(vote_type, page, per_page);
-  const initialData = await initialRowData.json();
-  return initialData;
-}
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const vote_type = req.query.vote_type || '';
-    const page = req.query.page || 0;
-    const per_page = req.query.per_page;
-    const result = await test(vote_type, page, per_page);
+    const vote_type = String(req.query.vote_type) || '';
+    const page = Number(req.query.page) || 0;
+    const per_page = Number(req.query.per_page);
+    const response = await getVotes(vote_type, page, per_page);
+    const result = await response.json();
     res.status(200).json(result);
   } catch (err) {
     res.status(500).json({ error: 'failed to load data' });
