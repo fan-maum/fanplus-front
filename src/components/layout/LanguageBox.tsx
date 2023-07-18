34fc392566696b2e947fd2c2b52d4a55aaafee7c
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-07-18 09:35:49 +0900

    style: 언어 선택자 우측 화살표 아이콘 추가 * 사이드바일 때는 화살표가 반대쪽 끝으로 가게 정렬

--- src/components/layout/LanguageBox.tsx
+++ src/components/layout/LanguageBox.tsx
@@ -38,11 +38,31 @@ const LanguageBox = ({ language }: { language: string }) => {
             height: '24px',
             margin: '0px',
             padding: '0px',
+            justifyContent: 'space-between',
           },
         }}
       >
-        <img src="/icons/언어팩.svg" css={{ marginRight: '7px', width: '14px', height: '14px' }} />
-        {language}
+        <div css={{ display: 'flex', alignItems: 'center' }}>
+          <img
+            src="/icons/언어팩.svg"
+            css={{
+              margin: '0px 5px 2px 0px',
+              width: '14px',
+              height: '14px',
+              '@media(max-width:991px)': { margin: '0px 5px 0px 0px' },
+            }}
+          />
+          <p>{language}</p>
+        </div>
+        <img
+          src="/icons/icon_down.svg"
+          css={{
+            margin: '0px 5px 1px',
+            width: '11px',
+            height: '6px',
+            '@media(max-width: 991px)': { margin: '0px 5px' },
+          }}
+        />
       </div>
       {isHovered && <LanguageContainer />}
     </li>
