e29648225d94bef9f2bb9d41812439151ff36d0c
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-01 15:43:27 +0900

    수정: console 로그 창으로 인한 빌드 오류 수정

--- src/pages/api/auth/callback/apple.ts
+++ src/pages/api/auth/callback/apple.ts
@@ -4,8 +4,8 @@ import axios from 'axios';
 const appleLoginHandler: NextApiHandler = async (req, res) => {
   const authorizationCode = req.query.code;
   const nextUrl = (req.query.state as string).replaceAll(';', '&');
-  console.log(authorizationCode);
-  console.log(nextUrl);
+  // console.log(authorizationCode);
+  // console.log(nextUrl);
 
   // console.log(userInfo.data);
   // res.setHeader("Set-Cookie", `USER_INFO=${userInfo.data}; Path=/; HttpOnly`);
