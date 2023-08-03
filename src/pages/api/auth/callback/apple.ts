555ebbfffcc92cd3eff6b6033d64a75b098149c6
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-03 18:06:38 +0900

    test: apple 로그인 테스트(확인용 console)

--- src/pages/api/auth/callback/apple.ts
+++ src/pages/api/auth/callback/apple.ts
@@ -3,12 +3,16 @@ import axios from 'axios';
 
 const appleLoginHandler: NextApiHandler = async (req, res) => {
   const authorizationCode = req.query.code;
+  // eslint-disable-next-line no-console
+  console.log(authorizationCode);
+
   const nextUrl = (req.query.state as string).replaceAll(';', '&');
   let user_lang = nextUrl.split('/')[3];
   if (user_lang === 'in') user_lang = 'id';
   else if (user_lang === 'zh-CN') user_lang = 'zh';
   else if (user_lang === 'zh-TW') user_lang = 'zhtw';
-
+  // eslint-disable-next-line no-console
+  console.log(nextUrl);
   const result = await axios.post(
     'https://napi.appphotocard.com/voteWeb/auth/apple',
     {
@@ -19,6 +23,8 @@ const appleLoginHandler: NextApiHandler = async (req, res) => {
     },
     { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
   );
+  // eslint-disable-next-line no-console
+  console.log(result.data);
   if (result.data.RESULTS.MSG === 'success') {
     res.setHeader(`Set-Cookie`, `logined=yes; Path=/; HttpOnly`);
     res.setHeader(
@@ -26,6 +32,8 @@ const appleLoginHandler: NextApiHandler = async (req, res) => {
       `onboarded=${result.data.RESULTS.DATAS.ONBOARDING_FIN_YN}; Path=/; HttpOnly`
     );
   }
+  // eslint-disable-next-line no-console
+  console.log('리디렉 직전ㄴ');
 
   // console.log(authorizationCode);
   // console.log(nextUrl);
