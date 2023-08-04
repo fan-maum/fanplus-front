52d3508ed4c910e2287fb4c8aa0a35737f95d8dc
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-04 09:02:02 +0900

    test: apple 로그인 테스트(값 읽어오는 로직 수정)

--- src/pages/api/auth/callback/apple.ts
+++ src/pages/api/auth/callback/apple.ts
@@ -2,29 +2,31 @@ import { NextApiHandler } from 'next';
 import axios from 'axios';
 
 const appleLoginHandler: NextApiHandler = async (req, res) => {
-  const authorizationCode = req.body.code;
-  // eslint-disable-next-line no-console
-  console.log(authorizationCode);
+  const payload = req.body;
+  const paramSets = payload.split('&');
+  let paramObjects: { state?: string; code?: string } = {};
+  for (let i = 0; i < paramSets.length; i++) {
+    paramObjects[paramSets[i].split('=')[0]] = paramSets[i].split('=')[1];
+  }
+  const nextUrl = decodeURIComponent(paramObjects['state'] as string).replaceAll(';', '&');
+  const authCode = paramObjects['code'];
 
-  const nextUrl = (req.query.state as string).replaceAll(';', '&');
   let user_lang = nextUrl.split('/')[3];
   if (user_lang === 'in') user_lang = 'id';
   else if (user_lang === 'zh-CN') user_lang = 'zh';
   else if (user_lang === 'zh-TW') user_lang = 'zhtw';
-  // eslint-disable-next-line no-console
-  console.log(nextUrl);
+
   const result = await axios.post(
     'https://napi.appphotocard.com/voteWeb/auth/apple',
     {
       platform: 'web',
-      code: authorizationCode,
+      code: authCode,
       timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
       user_lang: user_lang,
     },
     { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
   );
-  // eslint-disable-next-line no-console
-  console.log(result.data);
+
   if (result.data.RESULTS.MSG === 'success') {
     res.setHeader(`Set-Cookie`, `logined=yes; Path=/; HttpOnly`);
     res.setHeader(
@@ -32,14 +34,6 @@ const appleLoginHandler: NextApiHandler = async (req, res) => {
       `onboarded=${result.data.RESULTS.DATAS.ONBOARDING_FIN_YN}; Path=/; HttpOnly`
     );
   }
-  // eslint-disable-next-line no-console
-  console.log('리디렉 직전ㄴ');
-
-  // console.log(authorizationCode);
-  // console.log(nextUrl);
-
-  // console.log(userInfo.data);
-  // res.setHeader("Set-Cookie", `USER_INFO=${userInfo.data}; Path=/; HttpOnly`);
   res.redirect(nextUrl);
   // res.redirect("/");
 };
