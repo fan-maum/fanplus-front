933378e926b2f74fc3053605a132459fefe73612
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-04 15:48:39 +0900

    수정: APPLE 환경변수로 변경

--- src/pages/api/auth/login/apple.ts
+++ src/pages/api/auth/login/apple.ts
@@ -2,12 +2,11 @@ import { NextApiHandler } from 'next';
 
 const handler: NextApiHandler = (req, res) => {
   const nextUrl = req.query.nextUrl || '/';
-  const domain = 'https://fanplus-front-git-fanmaum-front.vercel.app';
 
   res.redirect(
     'https://appleid.apple.com/auth/authorize?' +
-      `client_id=com.photocard.master.auth` +
-      `&redirect_uri=${domain}/api/auth/callback/apple/` +
+      `client_id=${process.env.APPLE_CLIENT_ID}` +
+      `&redirect_uri=${process.env.APPLE_REDIRECT_URI}` +
       '&response_type=code' +
       '&scope=name%20email' +
       '&response_mode=form_post' +
