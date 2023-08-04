f94faf18a23b12ca84b312cde4e7bff13cdff84e
Kyeongeun Park <kellypark2032@gmail.com>
2023-08-04 15:32:53 +0900

    test

--- src/components/atoms/BackButton.tsx
+++ src/components/atoms/BackButton.tsx
@@ -10,7 +10,7 @@ function BackButton({ ...props }: Props) {
       <Image
         fill
         css={{ verticalAlign: 'middle' }}
-        src="/icons/icon_backArrow.svg"
+        src="/icons/icon_share.svg"
         alt="icon_backArrow"
       />
     </UnstyledButton>
