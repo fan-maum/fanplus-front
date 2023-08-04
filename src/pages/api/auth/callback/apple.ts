9980344090f6d889178438c04b8b9fb6b6fc3eda
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-04 10:35:31 +0900

    test: apple 로그인 테스트(backend api 재연결)

--- src/pages/api/auth/callback/apple.ts
+++ src/pages/api/auth/callback/apple.ts
@@ -2,38 +2,33 @@ import { NextApiHandler } from 'next';
 import axios from 'axios';
 
 const appleLoginHandler: NextApiHandler = async (req, res) => {
-  console.log(req);
-  const authorizationCode = req.body;
-  // console.log(authorizationCode);
-  const callbackURL = req.body.state;
-  console.log('body: ', req.body);
-  // const nextUrl = (req.query.state as string).replaceAll(';', '&');
+  const authorizationCode = req.body.code;
+  const nextUrl = encodeURIComponent(req.body.state).replaceAll(';', '&');
 
-  // let user_lang = nextUrl.split('/')[3];
-  // if (user_lang === 'in') user_lang = 'id';
-  // else if (user_lang === 'zh-CN') user_lang = 'zh';
-  // else if (user_lang === 'zh-TW') user_lang = 'zhtw';
+  let user_lang = nextUrl.split('/')[3];
+  if (user_lang === 'in') user_lang = 'id';
+  else if (user_lang === 'zh-CN') user_lang = 'zh';
+  else if (user_lang === 'zh-TW') user_lang = 'zhtw';
 
-  // const result = await axios.post(
-  //   'https://napi.appphotocard.com/voteWeb/auth/apple',
-  //   {
-  //     platform: 'web',
-  //     code: authorizationCode,
-  //     timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-  //     user_lang: user_lang,
-  //   },
-  //   { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
-  // );
+  const result = await axios.post(
+    'https://napi.appphotocard.com/voteWeb/auth/apple',
+    {
+      platform: 'web',
+      code: authorizationCode,
+      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      user_lang: user_lang,
+    },
+    { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+  );
 
-  // if (result.data.RESULTS.MSG === 'success') {
-  //   res.setHeader(`Set-Cookie`, `logined=yes; Path=/; HttpOnly`);
-  //   res.setHeader(
-  //     'Set-Cookie',
-  //     `onboarded=${result.data.RESULTS.DATAS.ONBOARDING_FIN_YN}; Path=/; HttpOnly`
-  //   );
-  // }
-  // res.redirect(nextUrl);
-  res.redirect(302, 'https://google.com');
+  if (result.data.RESULTS.MSG === 'success') {
+    res.setHeader(`Set-Cookie`, `logined=yes; Path=/; HttpOnly`);
+    res.setHeader(
+      'Set-Cookie',
+      `onboarded=${result.data.RESULTS.DATAS.ONBOARDING_FIN_YN}; Path=/; HttpOnly`
+    );
+  }
+  res.redirect(302, nextUrl);
 };
 
 export default appleLoginHandler;
