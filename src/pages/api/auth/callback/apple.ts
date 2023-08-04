24af2b4913ef283aab52468614eb8dab7d57b809
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-04 11:35:43 +0900

    test: apple 로그인(backend 재추가)

--- src/pages/api/auth/callback/apple.ts
+++ src/pages/api/auth/callback/apple.ts
@@ -10,24 +10,24 @@ const appleLoginHandler: NextApiHandler = async (req, res) => {
   // else if (user_lang === 'zh-CN') user_lang = 'zh';
   // else if (user_lang === 'zh-TW') user_lang = 'zhtw';
 
-  // const result = await axios.post(
-  //   'https://napi.appphotocard.com/voteWeb/auth/apple',
-  //   {
-  //     platform: 'web',
-  //     code: authorizationCode,
-  //     timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-  //     user_lang: 'ko',
-  //   },
-  //   { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
-  // );
+  const result = await axios.post(
+    'https://napi.appphotocard.com/voteWeb/auth/apple',
+    {
+      platform: 'web',
+      code: authorizationCode,
+      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      user_lang: 'ko',
+    },
+    { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+  );
 
-  // if (result.data.RESULTS.MSG === 'success') {
-  //   res.setHeader(`Set-Cookie`, `logined=yes; Path=/; HttpOnly`);
-  //   res.setHeader(
-  //     'Set-Cookie',
-  //     `onboarded=${result.data.RESULTS.DATAS.ONBOARDING_FIN_YN}; Path=/; HttpOnly`
-  //   );
-  // }
+  if (result.data.RESULTS.MSG === 'success') {
+    res.setHeader(`Set-Cookie`, `logined=yes; Path=/; HttpOnly`);
+    res.setHeader(
+      'Set-Cookie',
+      `onboarded=${result.data.RESULTS.DATAS.ONBOARDING_FIN_YN}; Path=/; HttpOnly`
+    );
+  }
   res.redirect(302, nextUrl);
 };
 
