bfd546fff87f146bec95e9133927a6090cc9e79b
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-07 08:35:15 +0900

    build: 구글 로그인 성공시 쿠키값 세팅

--- src/pages/api/auth/callback/google.ts
+++ src/pages/api/auth/callback/google.ts
@@ -5,7 +5,7 @@ const googleLoginHandler: NextApiHandler = async (req, res) => {
   const authorizationCode = req.query.code;
   const nextUrl = (req.query.state as string).replaceAll(';', '&');
 
-  const response = await axios.post(
+  const googleResponse = await axios.post(
     `https://oauth2.googleapis.com/token`,
     {
       code: authorizationCode,
@@ -16,10 +16,9 @@ const googleLoginHandler: NextApiHandler = async (req, res) => {
     },
     { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
   );
-  const id_token = response.data.id_token;
+  const id_token = googleResponse.data.id_token;
 
-  // 만약 오류가 난다면 아마 ko 때문일 것. (나중에 develop이랑 머지하면 해결될 문제임!)
-  const result = await axios.post(
+  const backResponse = await axios.post(
     'https://napi.appphotocard.com/voteWeb/auth/google',
     {
       platform: 'web',
@@ -30,12 +29,10 @@ const googleLoginHandler: NextApiHandler = async (req, res) => {
     { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
   );
 
-  if (result.data.RESULTS.MSG === 'success') {
-    res.setHeader(`Set-Cookie`, `logined=yes; Path=/; HttpOnly`);
-    res.setHeader(
-      'Set-Cookie',
-      `onboarded=${result.data.RESULTS.DATAS.ONBOARDING_FIN_YN}; Path=/; HttpOnly`
-    );
+  const results = backResponse.data.RESULTS;
+  if (results.MSG === 'success') {
+    res.setHeader(`Set-Cookie`, `user_id=${results.DATA.USER_IDENTITY}; Path=/; HttpOnly`);
+    res.setHeader('Set-Cookie', `onboarding=${results.DATAS.ONBOARDING_FIN_YN}; Path=/; HttpOnly`);
   }
   res.redirect(nextUrl);
 };
