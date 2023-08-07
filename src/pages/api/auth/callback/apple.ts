5865abeaa414db69e8816f2a56e43710db2ae813
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-07 09:49:17 +0900

    test: apple 로그인 테스트(값을 잘 받아오는지)

--- src/pages/api/auth/callback/apple.ts
+++ src/pages/api/auth/callback/apple.ts
@@ -7,27 +7,28 @@ const appleLoginHandler: NextApiHandler = async (req, res) => {
   const authorizationCode = req.body.code;
   const nextUrl = (req.body.state as string).replaceAll(';', '&');
 
-  const negotiator = new Negotiator({
-    headers: { 'accept-language': req.headers['accept-language'] },
-  });
-  const user_lang = negotiator.language(SUPPORT_LANGUAGE) || 'en';
-  const backResponse = await axios.post(
-    'https://napi.appphotocard.com/voteWeb/auth/apple',
-    {
-      platform: 'web',
-      code: authorizationCode,
-      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-      user_lang: user_lang,
-    },
-    { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
-  );
+  // const negotiator = new Negotiator({
+  //   headers: { 'accept-language': req.headers['accept-language'] },
+  // });
+  // const user_lang = negotiator.language(SUPPORT_LANGUAGE) || 'en';
+  // const backResponse = await axios.post(
+  //   'https://napi.appphotocard.com/voteWeb/auth/apple',
+  //   {
+  //     platform: 'web',
+  //     code: authorizationCode,
+  //     timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+  //     user_lang: user_lang,
+  //   },
+  //   { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+  // );
 
-  const results = backResponse.data.RESULTS;
-  if (results.MSG === 'success') {
-    res.setHeader(`Set-Cookie`, `user_id=${results.DATAS.USER_IDENTITY}; Path=/; HttpOnly`);
-    res.setHeader('Set-Cookie', `onboarding=${results.DATAS.ONBOARDING_FIN_YN}; Path=/; HttpOnly`);
-  }
-  res.redirect(302, nextUrl);
+  // const results = backResponse.data.RESULTS;
+  // if (results.MSG === 'success') {
+  //   res.setHeader(`Set-Cookie`, `user_id=${results.DATAS.USER_IDENTITY}; Path=/; HttpOnly`);
+  //   res.setHeader('Set-Cookie', `onboarding=${results.DATAS.ONBOARDING_FIN_YN}; Path=/; HttpOnly`);
+  // }
+  res.setHeader('Set-Cookie', `auth_code=${authorizationCode}; Path=/; HttpOnly`);
+  res.redirect(nextUrl);
 };
 
 export default appleLoginHandler;
