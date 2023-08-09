c7dbd94985908b4459d7f49d43a91237a93ace02
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-09 14:14:36 +0900

    test: apple 로그인...

--- src/pages/api/auth/login/apple.ts
+++ src/pages/api/auth/login/apple.ts
@@ -6,7 +6,7 @@ const handler: NextApiHandler = (req, res) => {
   res.redirect(
     'https://appleid.apple.com/auth/authorize?' +
       `client_id=${process.env.APPLE_CLIENT_ID}` +
-      `&redirect_uri=${process.env.APPLE_REDIRECT_URI}` +
+      `&redirect_uri=${process.env.APPLE_REDIRECT_URI}/` +
       '&response_type=code' +
       '&scope=name%20email' +
       '&response_mode=form_post' +
