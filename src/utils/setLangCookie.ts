1997ddbe00834035a2d5ab2410b18e1718dc1857
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-10 13:45:00 +0900

    build: 다국어 처리 완료(domain 다시 추가)

--- src/utils/setLangCookie.ts
+++ src/utils/setLangCookie.ts
@@ -4,6 +4,6 @@ export const setLangCookie = (language: LangCookie) => {
   const expirationDate = new Date();
   expirationDate.setDate(expirationDate.getDate() + 365);
   const domain = process.env.COOKIE_DOMAIN || '.localhost';
-  const cookieString = `USER_LANG=${language}; expires=${expirationDate.toUTCString()}; path=/;`;
+  const cookieString = `USER_LANG=${language}; expires=${expirationDate.toUTCString()}; path=/; domain=${domain};`;
   document.cookie = cookieString;
 };
