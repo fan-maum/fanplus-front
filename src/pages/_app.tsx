18b2aef342017de5b8f9061567471fbea99f60f2
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-11 16:31:02 +0900

    feat: 투표 페이지 SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import Script from 'next/script';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { DefaultSeo } from 'next-seo';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,6 +25,23 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
+        <DefaultSeo
+          title="Fanplus"
+          description="팬플러스는 최애가 같은 팬들을 대상으로 다양한 콘텐츠와 소통 기능을 제공하는 종합 덕질 앱이에요. 유일하게 유료 결제 없는 아이돌 생일/기념일 투표를 진행하고 있답니다."
+          openGraph={{
+            title: '서비스 소개',
+            type: 'website',
+            url: 'https://fanplus.co.kr',
+            siteName: '팬플러스(fanPlus)',
+            description:
+              '팬플러스는 최애가 같은 팬들을 대상으로 다양한 콘텐츠와 소통 기능을 제공하는 종합 덕질 앱이에요. 유일하게 유료 결제 없는 아이돌 생일/기념일 투표를 진행하고 있답니다.',
+            images: [
+              {
+                url: 'https://fanplus.co.kr/wp-content/uploads/2020/04/fanplus_metatag.png',
+              },
+            ],
+          }}
+        />
         <Script
           src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
           integrity="sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx"
