2fb7d88bfa2f9239994c2ef2b57949bd53c09de1
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-11 13:37:47 +0900

    수정: 쿠키 domain 삭제

--- src/utils/setLangCookie.ts
+++ src/utils/setLangCookie.ts
@@ -3,7 +3,6 @@ export type LangCookie = 'ko' | 'en' | 'es' | 'in' | 'ja' | 'vi' | 'zh-CN' | 'zh
 export const setLangCookie = (language: LangCookie) => {
   const expirationDate = new Date();
   expirationDate.setDate(expirationDate.getDate() + 365);
-  const domain = process.env.COOKIE_DOMAIN || '.localhost';
-  const cookieString = `USER_LANG=${language}; expires=${expirationDate.toUTCString()}; path=/; domain=${domain};`;
+  const cookieString = `USER_LANG=${language}; expires=${expirationDate.toUTCString()}; path=/;`;
   document.cookie = cookieString;
 };
