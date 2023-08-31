df603e0e04712d371beaced0d3c0bba2f5cc814b
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-08-31 14:40:14 +0900

    modify: CommunityBoardWraper의 title prop을 선택적으로 변경하여 확장성 높임

--- src/components/organisms/CommunityBoardWrapper.tsx
+++ src/components/organisms/CommunityBoardWrapper.tsx
@@ -2,14 +2,14 @@ import { ListItemType } from '@/types/community';
 import CommunityBoardItem from '../molecules/CommunityBoardItem';
 
 type CommunityBoardWrapperType = {
-  title: string;
+  title?: string;
   boardList: ListItemType[];
 };
 
 const CommunityBoardWrapper = ({ title, boardList }: CommunityBoardWrapperType) => {
   return (
     <section css={{ marginBottom: '30px' }}>
-      <h4 css={{ margin: '15px 5px' }}>{title}</h4>
+      {title && <h4 css={{ margin: '15px 5px' }}>{title}</h4>}
       {boardList.map((boardItem) => {
         return (
           <CommunityBoardItem icon={boardItem.BOARD_ICON} title={boardItem.BOARD_TITLE} link="/" />
