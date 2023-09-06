569aeab07bf236a8fc4dfacec4746c80855026ad
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-09-06 17:12:37 +0900

    modify: 비로그인시 작업 사항 임시 주석처리

--- src/pages/api/community/withoutUserId/home.ts
+++ src/pages/api/community/withoutUserId/home.ts
@@ -1,25 +1,25 @@
-import { NextApiHandler } from 'next';
-import axios, { AxiosResponse } from 'axios';
-import type { CommunityHomeResponseTypeWithoutUserId } from '@/types/community';
+// import { NextApiHandler } from 'next';
+// import axios, { AxiosResponse } from 'axios';
+// import type { CommunityHomeResponseTypeWithoutUserId } from '@/types/community';
 
-const handler: NextApiHandler = async (req, res) => {
-  const lang = req.query.lang;
-  const origin = process.env.NEXT_PUBLIC_CLIENT_URL || 'https://dev.fanplus.co.kr';
+// const handler: NextApiHandler = async (req, res) => {
+//   const lang = req.query.lang;
+//   const origin = process.env.NEXT_PUBLIC_CLIENT_URL || 'https://dev.fanplus.co.kr';
 
-  try {
-    const response: AxiosResponse<CommunityHomeResponseTypeWithoutUserId> = await axios.get(
-      `https://napi.appphotocard.com/voteWeb/boards/0?lang=${lang}`,
-      {
-        headers: {
-          Origin: origin,
-          'Cache-Control': 'no-cache',
-        },
-      }
-    );
-    res.status(200).json(response.data);
-  } catch (error) {
-    res.status(500).json('failed to load data');
-  }
-};
+//   try {
+//     const response: AxiosResponse<CommunityHomeResponseTypeWithoutUserId> = await axios.get(
+//       `https://napi.appphotocard.com/voteWeb/boards/0?lang=${lang}`,
+//       {
+//         headers: {
+//           Origin: origin,
+//           'Cache-Control': 'no-cache',
+//         },
+//       }
+//     );
+//     res.status(200).json(response.data);
+//   } catch (error) {
+//     res.status(500).json('failed to load data');
+//   }
+// };
 
-export default handler;
+// export default handler;
