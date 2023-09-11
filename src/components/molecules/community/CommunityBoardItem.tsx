3239e28157573063fdbaff758c4630479333bbe3
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-09-11 10:59:17 +0900

    modify: 게시판 이미지 Image -> img

--- src/components/molecules/community/CommunityBoardItem.tsx
+++ src/components/molecules/community/CommunityBoardItem.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Link from 'next/link';
 
 export type BoardItemType = {
@@ -10,29 +9,19 @@ export type BoardItemType = {
 
 const CommunityBoardItem = ({ icon, title, link, onClickLocalStore }: BoardItemType) => {
   return (
-    <>
-      <Link
-        href={link}
-        css={{
-          display: 'flex',
-          borderBottom: '1px solid #d9d9d9',
-          margin: '5px 0px',
-          padding: '5px 5px 10px',
-        }}
-        onClick={onClickLocalStore}
-      >
-        <div css={{ position: 'relative', height: '84px', aspectRatio: '1/1' }}>
-          <Image
-            src={icon}
-            fill
-            sizes={'66px 84px'}
-            css={{ width: '100%', height: '100%' }}
-            alt={title}
-          />
-        </div>
-        <p css={{ margin: 'auto 5px', fontWeight: '500' }}>{title}</p>
-      </Link>
-    </>
+    <Link
+      href={link}
+      css={{
+        display: 'flex',
+        borderBottom: '1px solid #d9d9d9',
+        margin: '5px 0px',
+        padding: '5px 5px 10px',
+      }}
+      onClick={onClickLocalStore}
+    >
+      <img src={icon} css={{ width: '84px', height: '84px' }} alt={title} />
+      <p css={{ margin: 'auto 5px', fontWeight: '500' }}>{title}</p>
+    </Link>
   );
 };
 
