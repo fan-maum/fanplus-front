4df9dcb3568341042d09dd1782e45814c3875937
Kyeongeun Park <kellypark2032@gmail.com>
2023-10-06 15:05:03 +0900

    fix: post.ts logic

--- src/pages/api/community/post.ts
+++ src/pages/api/community/post.ts
@@ -5,16 +5,21 @@ import type { PostResponseType } from '@/types/community';
 const handler: NextApiHandler = async (req, res) => {
   const { boardIndex, postIndex, identity, lang } = req.query;
   const origin = process.env.NEXT_PUBLIC_CLIENT_URL || 'https://dev.fanplus.co.kr';
-  const unAuthUrl = `https://napi.appphotocard.com/voteWeb/boards/${boardIndex}/posts/${postIndex}/detail?lang=${lang}`;
-  const authUrl = `https://napi.appphotocard.com/voteWeb/boards/${boardIndex}/posts/${postIndex}/detail?identity=${identity}&lang=${lang}`;
-  const isIdentityUrl = identity === undefined ? unAuthUrl : authUrl;
+  const url = `https://napi.appphotocard.com/voteWeb/boards/${boardIndex}/posts/${postIndex}/detail`;
 
   try {
-    const response: AxiosResponse<PostResponseType> = await axios.get(isIdentityUrl, {
+    const response: AxiosResponse<PostResponseType> = await axios.get(url, {
       headers: {
         Origin: origin,
         'Cache-Control': 'no-cache',
       },
+      params:
+        identity === undefined
+          ? {
+              identity,
+              lang,
+            }
+          : { lang },
     });
     res.status(200).json(response.data);
   } catch (error) {
