c412cc4a145565dd787dc6a72c05342b6b1cf032
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-10-12 15:47:41 +0900

    build: main, login, signup, business, faq page string 이관

--- src/pages/[locale]/business.tsx
+++ src/pages/[locale]/business.tsx
@@ -1,13 +1,12 @@
 import Layout from '@/components/organisms/Layout';
 import BusinessTemplate from '@/components/templates/BusinessTemplate';
-import { BusinessText_KR } from '@/texts/ko';
 import type { UrlLangType } from '@/types/common';
 import type { GetServerSideProps } from 'next';
 
 const business = ({ urlLang }: { urlLang: UrlLangType }) => {
   return (
     <Layout urlLang={urlLang}>
-      <BusinessTemplate texts={BusinessText_KR} />
+      <BusinessTemplate urlLang={urlLang} />
     </Layout>
   );
 };
