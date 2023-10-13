ffbccbd05f4aada24b708ca7b4902a6f520253eb
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-10-13 11:30:40 +0900

    refactor: 불필요한 import문 제거

--- src/pages/[locale]/index.tsx
+++ src/pages/[locale]/index.tsx
@@ -1,6 +1,5 @@
 import Layout from '@/components/organisms/Layout';
 import MainPageTemplate from '@/components/templates/MainPageTemplate';
-import { mainPageTexts } from '@/texts/mainPageTexts';
 import type { UrlLangType } from '@/types/common';
 import type { GetServerSideProps } from 'next';
 import Head from 'next/head';
