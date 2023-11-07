fe358db62cba4de9a91f4ff7ec08091f57f930f8
Kyeongeun Park <kellypark2032@gmail.com>
2023-11-07 17:12:41 +0900

    feat: privacy.tsx / privacy_ko.tsx

---
+++ src/pages/[locale]/privacy.tsx
@@ -0,0 +1,334 @@
+import styled from '@emotion/styled';
+
+const Privacy = () => {
+  return (
+    <PrivacyWrapper>
+      <h1>Consent to Personal information collection</h1>
+      <Phrase>
+        <p>
+          FanPlus (Hereinafter referred to as &quot;Company&quot;) values user’s personal
+          information. This Privacy Policy specifies the policy
+          <br /> that is applied to the data collected by the &quot;Company&quot; through FanPlus
+          business (Hereinafter referred to as &quot;Service&quot;).
+        </p>
+      </Phrase>
+      <Phrase>
+        <p>
+          This Privacy Policy may be changed from time to time due to changes in government laws and
+          guidelines or changes in <br /> FanPlus internal policy. In cases where there are changes
+          to the contents of this Privacy Policy, the &quot;Company&quot; will <br /> promptly
+          reflect the changes in the Privacy Policy found on FanPlus App, and on the
+          &quot;Company&quot; official website.
+        </p>
+      </Phrase>
+      <div>
+        <Phrase style={{ paddingBottom: 20 }}>
+          <h3>1. Purpose and Category of personal information</h3>
+          <p>
+            The personal information is not processed for purposes other than the following
+            purposes, and is collected and stored <br /> after de-identification, so the data cannot
+            be identified as a specific individual.
+          </p>
+          <p>
+            In addition, if the purpose and catrgory of collecting personal information change, the
+            &quot;Company&quot; will implement necessary <br /> measures, such as obtaining an
+            additional user consent.
+          </p>
+          <p>
+            If an user starts using the &quot;Company&quot; service for the first time, the
+            &quot;Company&quot; may request the user to input essential <br /> information. If the
+            &quot;Company&quot; already has information, the user may not need to input the
+            information again, or the <br /> &quot;Company&quot; may provide a type of convenience
+            to deal with personal information.
+          </p>
+        </Phrase>
+        <table style={{ marginBottom: 46 }}>
+          <thead>
+            <tr>
+              <th>Purpose of Collecting Personal Information</th>
+              <th>Category of Required Personal Information</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <ul>
+                  <li>
+                    ① To provide general service such as sign-up, targeted AD, account managament,
+                    friends recommendation, etc.
+                  </li>
+                  <li>② To make statistics on the user service usage on mobile devices</li>
+                  <li>③ To offer prompt customer service</li>
+                  <li>④ To improve the user exprerience and offer a seamless service</li>
+                </ul>
+              </td>
+              <td>
+                <ul>
+                  <li>- Internet usage information: Website address, network status, etc.</li>
+                  <li>- Application information: Installing, executing, etc.</li>
+                  <li>- System information: OS or display information</li>
+                  <li>
+                    - Required: Email address (Google, Apple, Twitter, Kakao, etc.), service usage
+                    history, cookies
+                  </li>
+                  <li>
+                    - Optional: Mobile number (for mobile verification), birth date, gender,
+                    nationality
+                  </li>
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div style={{ marginBottom: 86 }}>
+        <h3>2. Personal information storing period</h3>
+        <Phrase>
+          <p>1. Sign-up: From the date of sign up to the date of account deactivation</p>
+        </Phrase>
+        <Phrase>
+          <p>However, in the cases below, the period may change:</p>
+        </Phrase>
+        <Phrase>
+          <p>- In case of legal investigation: until the investigation is closed</p>
+        </Phrase>
+        <Phrase>
+          <p>
+            - Information such us name, mobile number, account number, etc. may be kept until the
+            transaction/refund <br /> process ends
+          </p>
+        </Phrase>
+        <Phrase>
+          <p>
+            - Information such as name, mobile number, account number, financial transaction history
+            may be kept <br />
+            even after the account’s deactivation/withdrawal, to prevent fraudulent transaction and
+            money laundering, <br />
+            or confirm a refund account in case of payment cancellation, etc.
+          </p>
+        </Phrase>
+      </div>
+      <div>
+        <h3>3. Data security and storage</h3>
+        <Phrase style={{ paddingBottom: 0 }}>
+          <p>
+            The &quot;Company&quot; takes the security of customer information very seriously. The
+            &quot;Company&quot; maintains data security through <br /> industry standard security
+            measures, including the use of firewalls and encryption, and retains data for as long as
+            it meets <br /> the requirements below.
+          </p>
+        </Phrase>
+        <Phrase>
+          <ol>
+            <li>
+              <p>
+                <span>1&#41;</span>To complete the customer support services provided
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>2&#41;</span>To comply with applicable legal and regulatory obligations
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>3&#41;</span>To prevent unauthorized use or abuse of services
+              </p>
+            </li>
+            <li>
+              <p>
+                <span>4&#41;</span>To mediate disputes between users or any other conflicts
+              </p>
+            </li>
+          </ol>
+        </Phrase>
+        <Phrase style={{ paddingTop: 0, paddingBottom: 0 }}>
+          <p>
+            Users can withdraw their consent to the ‘Terms’ by deleting the application at any time,
+            and if the user deletes the <br /> application, the “Company” will no longer collect
+            data. However, data collected before deletion and processed into a form <br /> that
+            cannot identify a specific individual is not deleted.
+          </p>
+        </Phrase>
+      </div>
+      <div>
+        <h3>4. Changes of Privacy Policy</h3>
+        <Phrase style={{ marginBottom: 46 }}>
+          <p>
+            The &quot;Company&quot; may change the personal information processing policy for
+            various reasons, such as improving the user&apos;s <br />
+            personal information processing procedure, reflecting changes to the &quot;Company&quot;
+            services, or complying with applicable <br />
+            laws. If the &quot;Company&quot; changes the privacy policy, the change is notified to
+            users on the service or by other reasonable <br />
+            means.
+          </p>
+        </Phrase>
+        <Phrase>
+          <p>Contact us for inquiry related to Privacy policy.</p>
+        </Phrase>
+        <Phrase>
+          <p>E-Mail : appfanplus@gmail.com</p>
+        </Phrase>
+        <Phrase>
+          <p>&lt;Notice Date &#58; 26 October 2022 / Enforcement Date &#58; 9 November 2022&gt;</p>
+        </Phrase>
+      </div>
+      <div>
+        <h2>Previous policy</h2>
+        <Phrase>
+          <ol>
+            <li>
+              <p>
+                <span>-</span>
+                <a href="https://www.google.com/url?q=https://privacy.fanplus.co.kr/old_privacy_ko.html&sa=D&source=editors&ust=1666770064158869&usg=AOvVaw1-PV2Dr7buxR572WWzT8Kb">
+                  April 16, 2020
+                </a>
+              </p>
+            </li>
+          </ol>
+        </Phrase>
+      </div>
+    </PrivacyWrapper>
+  );
+};
+
+export default Privacy;
+
+const PrivacyWrapper = styled.div`
+  padding: 10px 10px 72px 10px;
+
+  p {
+    background-color: #ffffff;
+    font-weight: 400;
+    text-decoration: none;
+    vertical-align: baseline;
+    font-size: 11pt;
+    font-family: 'Arial';
+    font-style: normal;
+    color: #666;
+
+    line-height: 1.5;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }
+  h1 {
+    color: #000000;
+    font-weight: 700;
+    text-decoration: none;
+    vertical-align: baseline;
+    font-size: 25.5pt;
+
+    line-height: 1.5;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+    padding-top: 12pt;
+    padding-bottom: 12pt;
+  }
+  h2 {
+    color: #777777;
+    font-weight: 700;
+    text-decoration: none;
+    vertical-align: baseline;
+    font-size: 18pt;
+
+    line-height: 1.2;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+    padding-top: 12pt;
+  }
+  h3 {
+    color: #777777;
+    font-weight: 700;
+    text-decoration: none;
+    vertical-align: baseline;
+    font-size: 16pt;
+
+    line-height: 1.5;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+    padding-top: 12pt;
+    padding-bottom: 12pt;
+  }
+  table {
+    display: table;
+    border-collapse: separate;
+    box-sizing: border-box;
+    text-indent: initial;
+    line-height: 1.5;
+    orphans: 2;
+    widows: 2;
+    font-weight: normal;
+    font-size: medium;
+    font-style: normal;
+    text-align: start;
+    border-spacing: 2px;
+    border-color: gray;
+    font-variant: normal;
+    border-spacing: 0;
+    border-collapse: collapse;
+    margin-right: auto;
+  }
+  th,
+  td {
+    border-right-style: solid;
+    padding: 5pt 5pt 5pt 5pt;
+    border-bottom-color: #000000;
+    border-top-width: 1pt;
+    border-right-width: 1pt;
+    border-left-color: #000000;
+    vertical-align: top;
+    border-right-color: #000000;
+    border-left-width: 1pt;
+    border-top-style: solid;
+    background-color: #f3f3f3;
+    border-left-style: solid;
+    border-bottom-width: 1pt;
+    width: 297.6pt;
+    border-top-color: #000000;
+    border-bottom-style: solid;
+    text-align: left;
+    color: #777;
+    padding: 5pt;
+  }
+  th {
+    font-size: 12pt;
+    font-weight: 700;
+    background-color: #f3f3f3;
+  }
+  td {
+    font-size: 11pt;
+    font-weight: normal;
+    background-color: #fff;
+    ul {
+      padding-top: 12pt;
+      padding-bottom: 12pt;
+    }
+  }
+  ol {
+    li {
+      padding-top: 12pt;
+      padding-bottom: 12pt;
+      margin-left: 36pt;
+      span {
+        padding-right: 10px;
+        color: #000000;
+        font-size: 11pt;
+      }
+    }
+  }
+  a {
+    color: #1155cc;
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`;
+
+const Phrase = styled.div`
+  padding-top: 12pt;
+  padding-bottom: 12pt;
+`;
