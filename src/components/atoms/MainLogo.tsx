85091109014c3307d0d892625cb1d0c58f90fd33
Kyeongeun Park <kellypark2032@gmail.com>
2023-12-21 14:46:01 +0900

    fix: img css => link css 로 옮김

--- src/components/atoms/MainLogo.tsx
+++ src/components/atoms/MainLogo.tsx
@@ -3,33 +3,32 @@ import Link from 'next/link';
 const MainLogo = () => {
   return (
     <>
-      <Link href={'/'}>
-        <img
-          src="/images/fanplus_logo_hor.png"
-          alt="Fanplus 로고"
-          css={{
-            margin: '0px 15px',
-            cursor: 'pointer',
-            '@media(max-width:768px)': {
-              display: 'none',
-            },
-          }}
-        />
-      </Link>
-      <Link href={'/'}>
-        <img
-          src="/images/fanplus_logo_small.png"
-          alt="Fanplus 로고"
-          css={{
+      <Link
+        href={'/'}
+        css={{
+          margin: '0px 15px',
+          cursor: 'pointer',
+          '@media(max-width:768px)': {
             display: 'none',
-            width: '26px',
-            margin: '0px 15px',
-            cursor: 'pointer',
-            '@media(max-width: 768px)': {
-              display: 'block',
-            },
-          }}
-        />
+          },
+        }}
+      >
+        <img src="/images/fanplus_logo_hor.png" alt="Fanplus 로고" />
+      </Link>
+      <Link
+        href={'/'}
+        css={{
+          display: 'none',
+          width: '26px',
+          height: '26px',
+          margin: '0px 15px',
+          cursor: 'pointer',
+          '@media(max-width: 768px)': {
+            display: 'block',
+          },
+        }}
+      >
+        <img css={{ width: 'inherit' }} src="/images/fanplus_logo_small.png" alt="Fanplus 로고" />
       </Link>
     </>
   );
