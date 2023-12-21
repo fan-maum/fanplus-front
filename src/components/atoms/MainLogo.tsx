c7a8ded335ae005d9cf9901670464b54a086bc12
Kyeongeun Park <kellypark2032@gmail.com>
2023-12-21 14:07:31 +0900

    fix: router.push => Link 변경

--- src/components/atoms/MainLogo.tsx
+++ src/components/atoms/MainLogo.tsx
@@ -1,39 +1,36 @@
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const MainLogo = () => {
-  const router = useRouter();
   return (
     <>
-      <img
-        src="/images/fanplus_logo_hor.png"
-        alt="Fanplus 로고"
-        css={{
-          margin: '0px 15px',
-          cursor: 'pointer',
-          '@media(max-width:768px)': {
+      <Link href={'/'}>
+        <img
+          src="/images/fanplus_logo_hor.png"
+          alt="Fanplus 로고"
+          css={{
+            margin: '0px 15px',
+            cursor: 'pointer',
+            '@media(max-width:768px)': {
+              display: 'none',
+            },
+          }}
+        />
+      </Link>
+      <Link href={'/'}>
+        <img
+          src="/images/fanplus_logo_small.png"
+          alt="Fanplus 로고"
+          css={{
             display: 'none',
-          },
-        }}
-        onClick={() => {
-          router.push('/');
-        }}
-      />
-      <img
-        src="/images/fanplus_logo_small.png"
-        alt="Fanplus 로고"
-        css={{
-          display: 'none',
-          width: '26px',
-          margin: '0px 15px',
-          cursor: 'pointer',
-          '@media(max-width: 768px)': {
-            display: 'block',
-          },
-        }}
-        onClick={() => {
-          router.push('/');
-        }}
-      />
+            width: '26px',
+            margin: '0px 15px',
+            cursor: 'pointer',
+            '@media(max-width: 768px)': {
+              display: 'block',
+            },
+          }}
+        />
+      </Link>
     </>
   );
 };
