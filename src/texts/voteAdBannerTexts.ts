38cb3f674e5179e90a08571ff720794aa5482399
Kyeongeun Park <kellypark2032@gmail.com>
2023-12-28 15:12:54 +0900

    fix: 15표 => 5표로 변경

--- src/texts/voteAdBannerTexts.ts
+++ src/texts/voteAdBannerTexts.ts
@@ -3,35 +3,35 @@ import type { VoteDetailAdBannerTextType } from '@/types/textTypes';
 
 export const voteAdBannerTexts: LangStringType<VoteDetailAdBannerTextType> = {
   ko: {
-    message1: `아래 후보 중 내 최애에게 <span>매일 15표씩 무료로</span>`,
+    message1: `아래 후보 중 내 최애에게 <span>매일 5표씩 무료로</span>`,
     message2: `투표하고 광고 선물하세요!`,
   },
   en: {
-    message1: `<span>Use 15 free VTs to vote for</span>`,
+    message1: `<span>Use 5 free VTs to vote for</span>`,
     message2: `your favorite star to get AD rewards!`,
   },
   es: {
-    message1: `<span>¡Usa 15 VT gratis</span>`,
+    message1: `<span>¡Usa 5 VT gratis</span>`,
     message2: `para votar por tu estrella favorita!`,
   },
   in: {
-    message1: `<span>Gunakan 15 VT gratis</span>`,
+    message1: `<span>Gunakan 5 VT gratis</span>`,
     message2: `untuk memilih bintang favoritmu!`,
   },
   ja: {
-    message1: `<span>15個の無料VTを使用してお気に,</span>`,
+    message1: `<span>5個の無料VTを使用してお気に,</span>`,
     message2: `入りのスターに投票し、広告報酬を獲得しましょう！`,
   },
   vi: {
-    message1: `<span>Sử dụng 15 VT miễn</span>`,
+    message1: `<span>Sử dụng 5 VT miễn</span>`,
     message2: `phí để bầu cho ngôi sao yêu thích của bạn!`,
   },
   'zh-CN': {
-    message1: `<span>使用每日免费赠送的15票，</span>`,
+    message1: `<span>使用每日免费赠送的5票，</span>`,
     message2: `给最爱的偶像投票并给予明星广告礼物！`,
   },
   'zh-TW': {
-    message1: `<span>使用每日免費贈送的15票，</span>`,
+    message1: `<span>使用每日免費贈送的5票，</span>`,
     message2: `給最愛的偶像投票並給予明星廣告禮物`,
   },
 };
