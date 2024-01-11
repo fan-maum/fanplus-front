38fe1127f6f86e1223114134fe17bf63e023869f
Kyeongeun Park <kellypark2032@gmail.com>
2024-01-11 12:05:09 +0900

    fix: boardMobileTab 괄호 하나 삭제

--- src/components/organisms/community/mobile/BoardMobileTab.tsx
+++ src/components/organisms/community/mobile/BoardMobileTab.tsx
@@ -16,14 +16,14 @@ const BoardMobileTab = () => {
 export default BoardMobileTab;
 
 const BoardMobileTabWrapper: any = styled.div`
-    display: none;
-    @media screen and (max-width: 768px) {
+  display: none;
+  @media screen and (max-width: 768px) {
     display: none;
     /* display: flex;
-    align-items: center;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start; */
+      align-items: center;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start; */
 
     .boardMobileTabAllMenu-wrapper {
       width: 100%;
@@ -32,26 +32,25 @@ const BoardMobileTabWrapper: any = styled.div`
       align-items: center;
       @media (max-width: 768px) {
         display: none;
+      }
     }
   }
-    }
 
-    .menu-wrapper[data-active='true'] {
-      border-bottom: '2px solid var(--color-primary)';
-    }
+  .menu-wrapper[data-active='true'] {
+    border-bottom: '2px solid var(--color-primary)';
+  }
 
-    .menu-wrapper {
+  .menu-wrapper {
+    font-size: 14px;
+    color: #918f8f;
+    font-weight: 500;
+    padding: 10px 0;
+    cursor: pointer;
+
+    span[data-active='true'] {
+      color: 'var(--color-primary)';
       font-size: 14px;
-      color: #918f8f;
-      font-weight: 500;
-      padding: 10px 0;
-      cursor: pointer;
-
-      span[data-active='true'] {
-        color: 'var(--color-primary)';
-        font-size: 14px;
-        font-weight: bold;
-      }
+      font-weight: bold;
     }
   }
 `;
