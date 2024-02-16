9ecfdd22ca13a39adf60b0caff1fa58aeb6d9873
Kyeongeun Park <kellypark2032@gmail.com>
2024-02-16 16:33:13 +0900

    docs: voteModalTexts - 중국어 언어스트링 변경

--- src/texts/voteModalTexts.ts
+++ src/texts/voteModalTexts.ts
@@ -33,16 +33,16 @@ export const voteModalTexts: LangStringType<VoteModalFuncType> = {
       voteDoneFirst: `<b>${dailyTicketCount} voto(s)</b><br/>se han utilizado para votar para <span>${starName}.</span>`,
       voteDoneEnd: `¡Vota a la estrella con <b>${moreVoteCount} voto(s)</b><br/><b>gratis todos los días en Panplus!</b>`,
       voteBlockFirst: `Pueden votar una vez al día<br/>La hora estándar es 00:00 (hora coreana)`,
-      voteBlockEnd: `Si instalan la aplicación FanPlus se puede obtener  <b>${moreVoteCount}</b> voto(s) cada día`,
+      voteBlockEnd: `Si instalan la aplicación FanPlus se puede obtener <br/> <b>${moreVoteCount}</b> voto(s) cada día`,
     };
   },
   in: ({ dailyTicketCount, starName, moreVoteCount }) => {
     return {
-      voteProcess: `Apakah Anda ingin memilih <b>${dailyTicketCount}</b> suara secara gratis untuk <span>${starName}</span>?`,
+      voteProcess: `Apakah Anda ingin memilih <b>${dailyTicketCount}</b> suara<br/> secara gratis untuk <span>${starName}</span>?`,
       voteDoneFirst: `<b>${dailyTicketCount} suara</b> telah digunakan untuk memilih <br/>untuk <span>${starName}</span>`,
       voteDoneEnd: `<b>${moreVoteCount} setiap hari di aplikasi FanPlus.</b> <br/> Pilih dengan tiket gratis!`,
       voteBlockFirst: `Anda dapat memilih sekali per hari<br/>(gratis suara bebas diperbarui pukul 00:00 KST)`,
-      voteBlockEnd: `FanPlus app Instal aplikasinya.<br/>Anda mendapatkan <b>${moreVoteCount}</b> suara memberikan suara setiap hari`,
+      voteBlockEnd: `FanPlus app Instal aplikasinya.<br/>Anda mendapatkan <br/> <b>${moreVoteCount}</b> suara memberikan suara setiap hari`,
     };
   },
   ja: ({ dailyTicketCount, starName, moreVoteCount }) => {
@@ -56,7 +56,7 @@ export const voteModalTexts: LangStringType<VoteModalFuncType> = {
   },
   vi: ({ dailyTicketCount, starName, moreVoteCount }) => {
     return {
-      voteProcess: `Bạn có muốn bình chọn phiếu <b>${dailyTicketCount}</b> miễn phí cho <span>${starName}</span> không?`,
+      voteProcess: `Bạn có muốn bình chọn phiếu <b>${dailyTicketCount}</b> miễn<br/> phí cho <span>${starName}</span> không?`,
       voteDoneFirst: ` <span>Gửi ${starName}</span><br/><b>${dailyTicketCount}</b>phiếu bầu đã được bỏ phiếu.`,
       voteDoneEnd: `Hãy bình chọn thêm<br/><b>${moreVoteCount} mỗi ngày miễn phí</b><br/><b>trên ứng dụng FanPlus!</b>`,
       voteBlockFirst: `Một ngày chỉ có một lần được bình chọn<br/>(Phiếu bầu miễn phí sẽ được gia hạn vào lúc 00:00 KST)`,
@@ -65,7 +65,7 @@ export const voteModalTexts: LangStringType<VoteModalFuncType> = {
   },
   'zh-CN': ({ dailyTicketCount, starName, moreVoteCount }) => {
     return {
-      voteProcess: `您想给<span>${starName}</span>免费投票吗？`,
+      voteProcess: `致<span>${starName}</span>先生<br/>您要免费投票<b>${dailyTicketCount}</b>吗？`,
       voteDoneFirst: `给<span>${starName}</span><br/>投<b>${dailyTicketCount}票。</b>`,
       voteDoneEnd: `请在 <b>FanPlus 应用免费投${moreVoteCount}票</b>`,
       voteBlockFirst: `您每天只能参与一次投票。<br/>(免费投票00:00 KST重新设置)`,
@@ -74,7 +74,7 @@ export const voteModalTexts: LangStringType<VoteModalFuncType> = {
   },
   'zh-TW': ({ dailyTicketCount, starName, moreVoteCount }) => {
     return {
-      voteProcess: `您想給<span>${starName}</span>免費投票嗎？`,
+      voteProcess: `致<span>${starName}</span><br/>您確定要免費投票<b>${dailyTicketCount}</b>嗎？`,
       voteDoneFirst: `給<span>${starName}</span><br/>投<b>${dailyTicketCount}票。</b>`,
       voteDoneEnd: `请在 <b>FanPlus 應用免費投${moreVoteCount}票</b>`,
       voteBlockFirst: `您每天只能參與一次投票。<br/>(免費投票00:00 KST重新設置)`,
