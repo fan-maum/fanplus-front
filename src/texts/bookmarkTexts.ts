ae3406ddb47a1c08cdc58ad5d7a33949e66db2e6
Kyeongeun Park <kellypark2032@gmail.com>
2024-02-21 15:24:12 +0900

    fix: mobile QA - 8. 전체글, BEST 인기글(실시간) 차단할 경우 게시판 이동 수정

--- src/texts/bookmarkTexts.ts
+++ src/texts/bookmarkTexts.ts
@@ -10,49 +10,49 @@ export const bookmarkTexts: LangStringType<CommmunityBookmarkType> = {
     seeSideBar: '게시판 메뉴 보기',
   },
   en: {
-    canBookmarkMessage: ['자주 찾는 게시판을', '즐겨찾기 할 수 있습니다.'],
+    canBookmarkMessage: ['Bookmark your favorite boards', 'for easy access.'],
     NoBookmarkMessage: `<div>Tap on the <img src="/icons/icon_star.svg" alt="bookmark_icon" />
     icon to bookmark <br/> a board.</div>`,
     seeMore: 'See more',
     seeSideBar: 'See all boards',
   },
   es: {
-    canBookmarkMessage: ['자주 찾는 게시판을', '즐겨찾기 할 수 있습니다.'],
+    canBookmarkMessage: ['Marque sus tablas favoritas para', 'facilitar el acceso.'],
     NoBookmarkMessage: `<div>Bata en el icono <img src="/icons/icon_star.svg" alt="bookmark_icon" />
     para marcar <br/> un tablero.</div>`,
     seeMore: 'Ver más',
     seeSideBar: 'Ver todos los tableros',
   },
   in: {
-    canBookmarkMessage: ['자주 찾는 게시판을', '즐겨찾기 할 수 있습니다.'],
+    canBookmarkMessage: ['Tandai papan favorit Anda', 'untuk akses mudah.'],
     NoBookmarkMessage: `<div>Ketuk ikon <img src="/icons/icon_star.svg" alt="bookmark_icon" />
     untuk menandai <br/> suatu papan.</div>`,
     seeMore: 'Lihat lebih banyak',
     seeSideBar: 'Lihat semua papan',
   },
   ja: {
-    canBookmarkMessage: ['자주 찾는 게시판을', '즐겨찾기 할 수 있습니다.'],
+    canBookmarkMessage: ['お気に入りのボードをブックマークして', '簡単にアクセスできます。'],
     NoBookmarkMessage: `<div><img src="/icons/icon_star.svg" alt="bookmark_icon" />
     アイコンをタッ プしてボードを <br/> ブックマークします。</div>`,
     seeMore: '詳細を見る',
     seeSideBar: 'すべての掲示板を見る',
   },
   vi: {
-    canBookmarkMessage: ['자주 찾는 게시판을', '즐겨찾기 할 수 있습니다.'],
+    canBookmarkMessage: ['Đánh dấu các bảng yêu thích', 'của bạn để dễ dàng truy cập.'],
     NoBookmarkMessage: `<div>Nhấn vào biểu tượng <img src="/icons/icon_star.svg" alt="bookmark_icon" />
     để đánh <br/> dấu một bảng.</div>`,
     seeMore: 'Xem thêm',
     seeSideBar: 'Xem tất cả các bảng thông báo',
   },
   'zh-CN': {
-    canBookmarkMessage: ['자주 찾는 게시판을', '즐겨찾기 할 수 있습니다.'],
+    canBookmarkMessage: ['关注您最喜欢的消息面板', '以便于访问。'],
     NoBookmarkMessage: `<div>点击 <img src="/icons/icon_star.svg" alt="bookmark_icon" />
     即可为关注</div>`,
     seeMore: '查看详情',
     seeSideBar: '查看所有消息面板',
   },
   'zh-TW': {
-    canBookmarkMessage: ['자주 찾는 게시판을', '즐겨찾기 할 수 있습니다.'],
+    canBookmarkMessage: ['關注您最喜歡的訊息面板', '以便於訪問。'],
     NoBookmarkMessage: `<div>點擊 <img src="/icons/icon_star.svg" alt="bookmark_icon" />
     即可為關注</div>`,
     seeMore: '查看詳情',
